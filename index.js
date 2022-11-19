d4656486c90441ca3bd3c36e13b600da956a2a6b
cmacswan07 <cmacswan07@gmail.com>
2022-11-19 14:05:49 -0800

    added more gradient.

--- index.js
+++ index.js
@@ -11,6 +11,55 @@ const TICK = 30;
 const CELL_SIZE = 64;
 const PLAYER_SIZE = 10;
 
+let maxWallHeight = 0;
+
+const getHue = (hexColor) => {
+    let r = parseInt(hexColor.substr(1, 2), 16);
+    let g = parseInt(hexColor.substr(3, 2), 16);
+    let b = parseInt(hexColor.substr(5, 2), 16);
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    let max = Math.max(r, g, b)
+    let min = Math.min(r, g, b);
+    let h = (max + min) / 2;
+
+    if (max === min) {
+        h = s = 0;
+    } else {
+        let d = max - min;
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return h;
+}
+
+const getWallBrightness = (hexColor, wallHeight) => {
+    const hue = getHue(hexColor);
+    // the higher wallHeight is, the higher the brightness should be
+    // the smaller wallHeight is, the darker the brightness should be
+    // wallHeight 200 ( the closest wall line ) should return 100%
+    // wallHeight 0 ( the furthest wall line ) should return 0%
+    // wallHeight 160 should return 80%
+    // wallHeight 40 should return 20%
+    let lightValue = Math.floor((wallHeight / SCREEN_HEIGHT) * 100);
+
+    if (lightValue < 20) {
+        return 20;
+    } else if (lightValue > 60) {
+        return 60;
+    }
+    // console.log(lightValue, wallHeight, maxWallHeight);
+    return lightValue;
+}
+
 const getLinearGradientVertical = (color1, color2, color3) => {
     const linGrad = context.createLinearGradient(0, 0, 0, SCREEN_HEIGHT);
     linGrad.addColorStop(0, color1);
@@ -19,16 +68,30 @@ const getLinearGradientVertical = (color1, color2, color3) => {
     return linGrad;
 }
 
+const getLinearGradientHorizontal = (color1, color2, color3) => {
+    const linGrad = context.createLinearGradient(0, 0, SCREEN_WIDTH, 0);
+    linGrad.addColorStop(0, color1);
+    // linGrad.addColorStop(0.25, color2);
+    linGrad.addColorStop(0.5, color2);
+    // linGrad.addColorStop(0.75, color2);
+    linGrad.addColorStop(1, color1);
+    return linGrad;
+}
+
 const toRadians = (deg) => {
     return (deg * Math.PI) / 180;
 }
 
 const PLAYER_FOV = toRadians(60);
+
+// the shorter wallHeight, the lower the lightness should be
 const COLORS = {
     rays: '#fff',
     floor: ['#020024', '#090979', '#00D4FF'],
     ceiling: ['#00d4ff', '#097962', '#020024'],
-    wall: '#eb34c6',
+    wall: '#ff0095',
+    // wall: 'hsl(300, 76%, 50%)',
+    // wall: ['#e900ff', '#4f0551', '#e900ff'],
     wallDark: '#401838'
 };
 
@@ -177,7 +240,18 @@ const renderScene = (rays) => {
 
         // draw wall
         const wallHeight = (CELL_SIZE * 5) / distance * 277;
-        context.fillStyle = ray.vertical ? COLORS.wallDark : COLORS.wall;
+        maxWallHeight = Math.max(maxWallHeight, wallHeight);
+        // context.fillStyle = ray.vertical ? 
+        //     COLORS.wallDark 
+        // : 
+        //     COLORS.wall
+        // ;
+        let fillStyle = `hsl(${getHue(COLORS.wall)}, ${getWallBrightness(COLORS.wall, wallHeight)}%, ${getWallBrightness(COLORS.wall, wallHeight)}%)`
+        context.fillStyle = ray.vertical ? 
+            COLORS.wallDark 
+        : 
+            `hsl(${getHue(COLORS.wall)}, ${getWallBrightness(COLORS.wall, wallHeight)}%, ${getWallBrightness(COLORS.wall, wallHeight)}%)`
+        ;
         context.fillRect(i, SCREEN_HEIGHT / 2 - wallHeight / 2, 1, wallHeight);
 
         // draw floor
@@ -264,6 +338,8 @@ document.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowDown') {
         player.speed = -2;
     }
+
+    // need to move these to the gameLoop
 
     if (e.key === 'ArrowLeft') {
         player.angle -= .05;
